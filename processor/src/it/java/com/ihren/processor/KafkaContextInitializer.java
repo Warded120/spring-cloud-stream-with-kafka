f9f2944c7fac927136cc96c6fee525fd2e0518cb
Warded120 <greenvania11@gmail.com>
2025-04-24 16:10:33 +0300

    fixed configuration

--- processor/src/it/java/com/ihren/processor/KafkaContextInitializer.java
+++ processor/src/it/java/com/ihren/processor/KafkaContextInitializer.java
@@ -3,6 +3,7 @@ package com.ihren.processor;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.TestExecutionListener;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -10,6 +11,7 @@ import org.testcontainers.kafka.ConfluentKafkaContainer;
 
 import java.util.Map;
 
+@Configuration
 @Testcontainers
 public class KafkaContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>, TestExecutionListener {
 
@@ -17,25 +19,30 @@ public class KafkaContextInitializer implements ApplicationContextInitializer<Co
 
     @Override
     public void beforeTestClass(TestContext testContext) {
+        kafkaContainer.start();
     }
 
     @Override
     public void afterTestClass(TestContext testContext) {
+        kafkaContainer.destroy();
     }
 
     @Override
     public void prepareTestInstance(TestContext testContext) {
+        kafkaContainer.start();
     }
 
     @Override
     public void afterTestMethod(TestContext testContext) {
+        kafkaContainer.destroy();
     }
 
 
     @Override
     public void initialize(ConfigurableApplicationContext context) {
+        String bootstrapServers = kafkaContainer.getContainer().getBootstrapServers();
         var properties = Map.of(
-                "spring.kafka.bootstrap-servers", kafkaContainer.getContainer().getBootstrapServers()
+                "spring.kafka.bootstrap-servers", bootstrapServers
         );
 
         TestPropertyValues.of(properties)
--- processor/src/it/java/com/ihren/processor/PersonProcessorIT.java
+++ processor/src/it/java/com/ihren/processor/PersonProcessorIT.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 
 //TODO: extract these annotation to a custom @IntegrationTest annotation
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = KafkaContextInitializer.class)
+@TestExecutionListeners(listeners = KafkaContextInitializer.class)
 @ActiveProfiles("test")
 //TODO: add import() for KafkaTemplate and KafkaConsumer to produce and consume messages
 public class PersonProcessorIT {
