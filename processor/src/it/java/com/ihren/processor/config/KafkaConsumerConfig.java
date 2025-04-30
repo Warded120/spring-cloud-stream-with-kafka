52f9fc048ab087a3e5f8331d3a6aafb695f4250e
Warded120 <ihren@softserveinc.com>
2025-04-30 11:21:16 +0300

    fix: updated application.yml and application-test.yml

--- processor/src/it/java/com/ihren/processor/config/KafkaConsumerConfig.java
+++ processor/src/it/java/com/ihren/processor/config/KafkaConsumerConfig.java
@@ -12,16 +12,17 @@ import java.util.Map;
 
 @TestConfiguration
 public class KafkaConsumerConfig {
-    public static final String GROUP_ID = "consumer";
-
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
+
+    @Value("${spring.cloud.stream.bindings.processPerson-out-0.group}")
+    private String groupId;
 
     @Bean
     public KafkaConsumer<String, Person> kafkaConsumer() {
         Map<String, Object> configs = Map.of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-                ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID,
+                ConsumerConfig.GROUP_ID_CONFIG, groupId,
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, PersonDeserializer.class
         );
