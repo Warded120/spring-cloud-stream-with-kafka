5caf29d7e5d4ee6aba9237cce50f80cb8abfca78
Warded120 <ihren@softserveinc.com>
2025-05-22 15:26:45 +0300

    added outputItemEnricher

--- src/main/java/com/ihren/processor/enricher/ItemEnricher.java
+++ src/main/java/com/ihren/processor/enricher/ItemEnricher.java
@@ -11,16 +11,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ItemEnricher implements Enricher<OutputItem> {
     private final ClientService clientService;
-    //TODO: enricher itself must use mapStruct
+    //TODO: enricher itself must use mapStruct (?)
     private final ItemConverter converter;
 
     @Override
     public OutputItem enrich(OutputItem item) {
         ItemResponse itemResponse = clientService.getByItemId(item.id());
-        return doEnrich(item, itemResponse);
-    }
-
-    private OutputItem doEnrich(OutputItem item, ItemResponse itemResponse) {
+        //FIXME: why do I need to combine logic from enricher and converter? we had an opposite requirement
         return converter.convert(item, itemResponse);
     }
 }
