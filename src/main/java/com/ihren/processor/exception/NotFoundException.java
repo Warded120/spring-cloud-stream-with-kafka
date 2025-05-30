142766e2a8efbcadcd2bb0d28b833ff61ddd36e6
Warded120 <ihren@softserveinc.com>
2025-05-30 17:45:20 +0300

    fix: use super() to construct an exception

--- src/main/java/com/ihren/processor/exception/NotFoundException.java
+++ src/main/java/com/ihren/processor/exception/NotFoundException.java
@@ -2,10 +2,6 @@ package com.ihren.processor.exception;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(final String message) {
-        this(message, null);
-    }
-
-    public NotFoundException(final String message, final Throwable cause) {
-        super(message, cause);
+        super(message);
     }
 }
