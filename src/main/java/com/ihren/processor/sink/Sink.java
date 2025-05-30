f34f8f9a421dc191f35dddbd461aada29771bd47
Warded120 <ihren@softserveinc.com>
2025-05-30 17:39:10 +0300

    added TODO

--- src/main/java/com/ihren/processor/sink/Sink.java
+++ src/main/java/com/ihren/processor/sink/Sink.java
@@ -1,5 +1,6 @@
 package com.ihren.processor.sink;
 
+//TODO: creare reply topic where messages will be sent to be processed again (trigger it with controller)
 public interface Sink {
     <T> void apply(T input, Throwable cause);
 }
