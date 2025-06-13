9b2aea636a53ea52a8c2c19c040fdcfb2eb486a7
Warded120 <ihren@softserveinc.com>
2025-06-13 18:23:59 +0300

    minor improvements

--- src/main/java/com/ihren/processor/exception/ApplicationException.java
+++ src/main/java/com/ihren/processor/exception/ApplicationException.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 public class ApplicationException extends RuntimeException {
-    protected final ErrorCode errorCode;
+    private final ErrorCode errorCode;
     public ApplicationException(String message, ErrorCode errorCode) {
         super(message);
         this.errorCode = errorCode;
--- src/main/java/com/ihren/processor/exception/ValidationException.java
+++ src/main/java/com/ihren/processor/exception/ValidationException.java
@@ -2,7 +2,7 @@ package com.ihren.processor.exception;
 
 import com.ihren.processor.constant.ErrorCode;
 
-public class ValidationException extends ApplicationException{
+public class ValidationException extends ApplicationException {
     public ValidationException(String message, ErrorCode errorCode) {
         super(message, errorCode);
     }
